efc8534c9dee1beec4908a6ea34b5d1665bacea3
dragoocho <deagoocho@gmail.com>
2021-04-19 18:45:20 +0900

    hoverImageContainer 완성

---
+++ js/hoverContainer.js
@@ -0,0 +1,34 @@
+const imgHover1 = document.querySelector(".imgHover1");
+const imgHover2 = document.querySelector(".imgHover2");
+const imgHover3 = document.querySelector(".imgHover3");
+const imgHover4 = document.querySelector(".imgHover4");
+const imgHover5 = document.querySelector(".imgHover5");
+const imgHover6 = document.querySelector(".imgHover6");
+const imgHover7 = document.querySelector(".imgHover7");
+const imgHover8 = document.querySelector(".imgHover8");
+const mainCardImg = document.querySelector(".main_card");
+
+imgHover1.addEventListener("mouseover", () => {
+  mainCardImg.style.backgroundImage = "url(/imeges/image_015.jpg)";
+});
+imgHover2.addEventListener("mouseover", () => {
+  mainCardImg.style.backgroundImage = "url(/imeges/image_020.jpg)";
+});
+imgHover3.addEventListener("mouseover", () => {
+  mainCardImg.style.backgroundImage = "url(/imeges/image_021.jpg)";
+});
+imgHover4.addEventListener("mouseover", () => {
+  mainCardImg.style.backgroundImage = "url(/imeges/image_022.jpg)";
+});
+imgHover5.addEventListener("mouseover", () => {
+  mainCardImg.style.backgroundImage = "url(/imeges/image_016.jpg)";
+});
+imgHover6.addEventListener("mouseover", () => {
+  mainCardImg.style.backgroundImage = "url(/imeges/image_017.jpg)";
+});
+imgHover7.addEventListener("mouseover", () => {
+  mainCardImg.style.backgroundImage = "url(/imeges/image_019.jpg)";
+});
+imgHover8.addEventListener("mouseover", () => {
+  mainCardImg.style.backgroundImage = "url(/imeges/image_018.jpg)";
+});
